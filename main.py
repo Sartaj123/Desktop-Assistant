fd80569632d4e0ce14a83bf575100770fdb0ea9b
Sartaj <sartaj123@gmail.com>
2024-06-22 16:24:02 +0530

    Updated Main

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hellow ! I am Desktop Assistant. How can help you ?')
+import pyttsx3 as tts  # Text to speach conversion
+import speech_recognition as sr 
+import datetime as dt
+import wikipedia as wp
+import webbrowser as wb
+import os
+
+# Takeing voice from my system
+engine = tts.init('sapi5')
+voices = engine.getProperty('voices')
+'''
+print(voices) # sapi5 has two List of voices
+print(type(voices)) # Type of the object is list
+print(voices[0].id) # Male voice
+print(voices[1].id) # Female voice
+'''
+
+engine.setProperty('voice',voices[0].id)  # For this project lets take Male voice for desktop assistant
+engine.setProperty('rate',150)            # Rate of Speed of audio
+
+# Create a speak function
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+speak('I am Saartaaj')
