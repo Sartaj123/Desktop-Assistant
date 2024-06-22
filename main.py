32918c6839cc8786b5b29a67a7fda547e16205a8
Sartaj <sartaj123@gmail.com>
2024-06-22 23:26:19 +0530

    Updated Main

--- main.py
+++ main.py
@@ -20,7 +20,30 @@ engine.setProperty('rate',150)            # Rate of Speed of audio
 
 # Create a speak function
 def speak(text):
+    ''' This function takes text and returns voice
+    Args : speak(_type_) String'''
     engine.say(text)
     engine.runAndWait()
 
-speak('I am Saartaaj')
+# Speach Recognization function
+def takeCommand():
+    ''' This function will recognize voice and return text'''
+    r = sr.Recognizer()
+    with sr.Microphone() as source:   # Open the microphone
+        print("Listening")
+        r.pause_threshold = 1       # Pause the threshold for 1 mili seconds
+        audio = r.listen(source)
+
+        try:
+            print('Recognizing...')
+            query = r.recognize_google(audio, language = 'en-in')
+            print(f'User said: {query}\n')
+        except Exception as e:
+            print('Could not listen properly : Please say it again !')
+            return None
+        return query
+
+
+# speak('We are developing Desktop Assistant Project')
+text = takeCommand()
+speak(text)
